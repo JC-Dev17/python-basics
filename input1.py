a2e6dd02c740c42a9286d5af4e758978228b02b0
joy <joydot17@gmail.com>
2026-05-04 15:15:58 +0530

    user input

--- input1.py
+++ input1.py
@@ -1,4 +1,18 @@
-first_No = int(input("Enter a number: "))
-second_No = int(input("Enter another number: "))
+# first_No = int(input("Enter a number: "))
+# second_No = int(input("Enter another number: "))
 
-print("Result: ", first_No + second_No) 
+# print("Result: ", first_No + second_No) 
+
+# meter = float(input("Enter the value in meter : "))
+# millimeters = meter *1000
+# print("Meters and millimeters : ",(meter, millimeters))
+
+days = int(input("Enter Days :"))
+Hours= int(input("Enter Hours :"))
+minute=int(input("Enter the minute :"))
+
+a=days*24*60*60
+b=Hours*60*60
+c=minute*60
+
+print ("Result : ", a+b+c)
