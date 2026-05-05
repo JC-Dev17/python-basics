ca93c27d5136c5b40e4b664582d589bfd980554a
joy <joydot17@gmail.com>
2026-05-05 07:14:15 +0530

    methods

--- tuple.py
+++ tuple.py
@@ -1,5 +1,10 @@
-colours =["red", "green", "blue"]
-print(len(colours))
+# colours =["red", "green", "blue"]
+# print(len(colours))
 
-x,y=(17,2)
-print(x)
+# x,y=(17,2)
+# print(x)
+
+coll=(20,20,20,11,20,25,26)
+print(coll.index(20))
+print(coll.count(20))
+print(len(coll))
